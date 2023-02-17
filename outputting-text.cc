02a2c07bc797ed85353af8c7fbd694da3d530f1f
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-02-17 20:28:07 +0000

    Create outputting-text.cc

---
+++ outputting-text.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Starting program..." << flush;
+	cout << "Hello there! Here are my favourite fruits!" << endl;
+	cout << "Banana. " << "Apple. " << "Orange. " << endl;
+	cout << "Well, goodbye I guess!" << endl;
+	return 0;
+}
