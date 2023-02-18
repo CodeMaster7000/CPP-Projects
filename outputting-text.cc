8e179004d35d00ea38075cb4fc171e5ebd820693
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-02-18 20:03:19 +0000

    Update outputting-text.cc

--- outputting-text.cc
+++ outputting-text.cc
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
 
 	cout << "Starting program..." << flush;
-	cout << "Hello there! Here are my favourite fruits!" << endl;
-	cout << "Banana. " << "Apple. " << "Orange. " << endl;
+	cout << "Hello there! These are my favourite fruits:" << endl;
+	cout << "Bananas, " << "apples " << "and oranges." << endl;
 	cout << "Well, goodbye I guess!" << endl;
 	return 0;
 }
