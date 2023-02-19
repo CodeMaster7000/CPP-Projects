69028f460525c1628c08d18a4a61d3991021093e
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-02-19 16:21:05 +0000

    Create integer-types.cc

---
+++ integer-types.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+
+	int value = -12345;
+	cout << value << endl;
+
+	cout << "Maximum integer value: "<< INT_MAX << endl;
+	cout << "Minimum integer value: "<< INT_MIN << endl;
+
+	long int lValue = 23473298472984;
+	cout << lValue << endl;
+
+	short int sValue = 23473;
+	cout << sValue << endl;
+
+	cout << "Size of integer: " << sizeof(int) << endl;
+	cout << "Size of short integer: " << sizeof(short int) << endl;
+
+	unsigned int uValue = 234234;
+	cout << uValue << endl;
+
+	return 0;
+}
