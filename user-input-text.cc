4bf830a62da9095d9ee0ac26a9da7b4175efcc0d
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-02-19 20:02:15 +0000

    Update and rename outputting-text.cc to user-input-text.cc

---
+++ user-input-text.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+        cout << "Enter your name: " << flush;
+        string input;
+        cin >> input;
+        cout << "Your name is: " << input << endl;
+
+        cout << "Enter your age: " << flush;
+        int value;
+        cin >> value
+        cout << "You are " << value << "years old." << endl;
+        
+        return 0; 
+} 
