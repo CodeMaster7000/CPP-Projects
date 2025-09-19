0790e0ed9aab7821a1e14c8beffdb689fe8e9322
CodeMaster7000 <CodeMaster7K@gmail.com>
2025-09-19 06:24:39 +0100

    Vowel_or_Consonant.cpp

---
+++ Vowel_or_Consonant.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    bool isLowercaseVowel, isUppercaseVowel;
+
+    cout << "Enter letter: ";
+    cin >> c;
+
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    if (!isalpha(c))
+      printf("Error: non-alphabetic character.");
+    else if (isLowercaseVowel || isUppercaseVowel)
+        cout << c << " is a vowel.";
+    else
+        cout << c << " is a consonant.";
+    return 0;
+}
